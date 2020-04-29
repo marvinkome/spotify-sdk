ebb92f218f9c45bcb2f9d063ab3363736c6acc94
Marvin Kome <marvinkome@gmail.com>
2020-04-29 22:13:52 +0100

    Add new api methods

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const axios = require("axios");
 const querystring = require("querystring");
-const { formatSongData } = require("./utils");
+const { chunk } = require("lodash");
+const {
+  formatSongData,
+  openBrowserForAuth,
+  readCache,
+  cacheCode
+} = require("./utils");
 
 class Spotify {
-  constructor(clientID, clientSecret) {
+  constructor(clientID, clientSecret, options = { authType: null, scope: "" }) {
     this.clientID = clientID;
     this.clientSecret = clientSecret;
     this.token = null;
@@ -12,6 +18,11 @@ class Spotify {
     });
 
     return (async () => {
+      if (options.authType === "user") {
+        await this.authorize_user(options.scope);
+        return this;
+      }
+
       await this.authorize();
       return this;
     })();
@@ -55,6 +66,64 @@ class Spotify {
     }
   }
 
+  async authorize_user(scope) {
+    // get refresh token from cache
+    const refresh_token = readCache("refresh_token");
+    let code = null;
+
+    const authKey = Buffer.from(
+      `${this.clientID}:${this.clientSecret}`
+    ).toString("base64");
+
+    if (!refresh_token) {
+      // go through normal auth
+      code = (await openBrowserForAuth(this.clientID, scope)).code;
+    }
+
+    // get access token
+    try {
+      const axiosData = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        querystring.stringify({
+          grant_type: code ? "authorization_code" : "refresh_token",
+          ...(code ? { code } : { refresh_token }),
+          redirect_uri: "http://localhost:8008/callback"
+        }),
+        {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${authKey}`
+          }
+        }
+      );
+
+      // set access token
+      const { access_token, refresh_token: new_token } = axiosData.data;
+
+      this.axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${access_token}`;
+
+      this.token = access_token;
+
+      // cache refresh token
+      if (new_token) {
+        cacheCode("refresh_token", new_token);
+      }
+    } catch (e) {
+      console.error(
+        "SPOTIFY ERROR:: Error during authorization:",
+        e.message,
+        (e.response || {}).data
+      );
+      console.error({
+        clientID: this.clientID,
+        clientSecret: this.clientSecret,
+        authKey
+      });
+    }
+  }
+
   /**
    * Search for a track and returns the first item found
    *
@@ -83,20 +152,141 @@ class Spotify {
   }
 
   /**
-   * Gets all songs in a spotify playlist
-   * @param {string} playlistId - The id of the playlist
-   * @param {boolean} formatted - Format track or send raw data with all playlist item date
+   * Get all playlist in by a user
+   * This method requires user authType
    *
+   * @param {string} title - The track title
+   * @param {string} artist - The track artist
+   * @param {boolean} formatted - Format track or send raw track data with all fields
+   *
+   * @returns {object} track details
    */
-  async getPlaylistTracks(playlistId, formatted = true) {
-    const songs = [];
+  async getUserPlaylists(formater) {
+    const playlists = [];
+    let next = null;
 
-    // make first request to get total pages count
-    const { items, total, next } = await this.makePlaylistRequest(playlistId);
+    // make first request to get total page count and next page
+    let { items, total, ...resp } = await this.makeUserPlaylistRequest();
+    next = resp.next;
 
     // set tracks
     for (let item of items) {
-      songs.push(formatted ? formatSongData(item.track) : item);
+      playlists.push(formater ? formater(item) : item);
+    }
+
+    // get the total number of page (default is 20 per page)
+    const total_pages = Math.ceil(total / 20);
+
+    // go through total pages and repeat process
+    if (total_pages > 1) {
+      for (let i = 2; i <= total_pages; i++) {
+        // make subsequent requests
+        const { items, ...resp } = await this.makeUserPlaylistRequest(next);
+        next = resp.next;
+
+        // set tracks
+        for (let item of items) {
+          playlists.push(formater ? formater(item) : item);
+        }
+      }
+    }
+
+    return playlists;
+  }
+
+  async getUserAlbums(formater) {
+    const playlists = [];
+    let next = null;
+
+    // make first request to get total page count and next page
+    let { items, total, ...resp } = await this.makeUserAlbumRequest();
+    next = resp.next;
+
+    // set tracks
+    for (let item of items) {
+      playlists.push(formater ? formater(item) : item);
+    }
+
+    // get the total number of page (default is 20 per page)
+    const total_pages = Math.ceil(total / 20);
+
+    // go through total pages and repeat process
+    if (total_pages > 1) {
+      for (let i = 2; i <= total_pages; i++) {
+        // make subsequent requests
+        const { items, ...resp } = await this.makeUserAlbumRequest(next);
+        next = resp.next;
+
+        // set tracks
+        for (let item of items) {
+          playlists.push(formater ? formater(item) : item);
+        }
+      }
+    }
+
+    return playlists;
+  }
+
+  /**
+   * Get all liked tracks in by a user
+   * This method requires user authType
+   *
+   * @param {string} title - The track title
+   * @param {string} artist - The track artist
+   * @param {boolean} formatted - Format track or send raw track data with all fields
+   *
+   * @returns {object} track details
+   */
+  async getUserLikedSongs(formater) {
+    const songs = [];
+    let next = null;
+
+    // make first request to get total page count and next page
+    let { items, total, ...resp } = await this.makeUserLikedSongsRequest();
+    next = resp.next;
+
+    // set tracks
+    for (let item of items) {
+      songs.push(formater ? formater(item) : item);
+    }
+
+    // get the total number of page (default is 20 per page)
+    const total_pages = Math.ceil(total / 20);
+
+    // go through total pages and repeat process
+    if (total_pages > 1) {
+      for (let i = 2; i <= total_pages; i++) {
+        // make subsequent requests
+        const { items, ...resp } = await this.makeUserLikedSongsRequest(next);
+        next = resp.next;
+
+        // set tracks
+        for (let item of items) {
+          songs.push(formater ? formater(item) : item);
+        }
+      }
+    }
+
+    return songs;
+  }
+
+  /**
+   * Gets all songs in a spotify playlist
+   * @param {string} playlistId - The id of the playlist
+   * @param {function} formater - A function that formats every item
+   *
+   */
+  async getPlaylistTracks(playlistId, formater = formatSongData) {
+    const songs = [];
+    let next = null;
+
+    // make first request to get total pages count
+    let { items, total, ...resp } = await this.makePlaylistRequest(playlistId);
+    next = resp.next;
+
+    // set tracks
+    for (let item of items) {
+      songs.push(formater ? formater(item.track) : item);
     }
 
     // get the total number of page (default is 100 per page)
@@ -106,11 +296,15 @@ class Spotify {
     if (total_pages > 1) {
       for (let i = 2; i <= total_pages; i++) {
         // make subsequent requests
-        const { items } = await this.makePlaylistRequest(playlistId, next);
+        const { items, ...resp } = await this.makePlaylistRequest(
+          playlistId,
+          next
+        );
+        next = resp.next;
 
         // set tracks
         for (let item of items) {
-          songs.push(formatSongData ? formatSongData(item.track) : item);
+          songs.push(formater ? formater(item.track) : item);
         }
       }
     }
@@ -123,15 +317,17 @@ class Spotify {
    * @param {string} albumId
    * @param {boolean} formatted
    */
-  async getAlbumTracks(albumId, formatted = true) {
+  async getAlbumTracks(albumId, formater = formatSongData) {
     const songs = [];
+    let next = null;
 
     // make first request to get total pages count
-    const { items, total, next } = await this.makeAlbumRequest(albumId);
+    let { items, total, ...resp } = await this.makeAlbumRequest(albumId);
+    next = resp.next;
 
     // set tracks
     for (let item of items) {
-      songs.push(formatted ? formatSongData(item) : item);
+      songs.push(formater ? formater(item) : item);
     }
 
     // get the total number of page (default is 100 per page)
@@ -141,11 +337,12 @@ class Spotify {
     if (total_pages > 1) {
       for (let i = 2; i <= total_pages; i++) {
         // make subsequent requests
-        const { items } = await this.makeAlbumRequest(albumId, next);
+        const { items, ...resp } = await this.makeAlbumRequest(albumId, next);
+        next = resp.next;
 
         // set tracks
         for (let item of items) {
-          songs.push(formatSongData ? formatSongData(item.track) : item);
+          songs.push(formater ? formater(item.track) : item);
         }
       }
     }
@@ -153,7 +350,64 @@ class Spotify {
     return songs;
   }
 
+  /**
+   * Get audio features of a track
+   * @param {string[]} trackIds - track ids
+   */
+  async getAudiosFeatures(trackIds) {
+    let audio_data = [];
+
+    // split tracks in chunks
+    const arrayOftrackIds = chunk(trackIds, 100);
+
+    for (let tracks of arrayOftrackIds) {
+      const data = await this.makeAudiosFeaturesRequest(tracks);
+      audio_data = audio_data.concat(data.audio_features);
+    }
+
+    return audio_data;
+  }
+
   // requests
+  async makeUserPlaylistRequest(link = null) {
+    try {
+      const { data } = await this.axios.get(link || `/me/playlists`);
+      return data;
+    } catch (e) {
+      console.error(
+        "SPOTIFY ERROR:: cannot get user playlists data:",
+        e.message
+      );
+      console.error(e.config);
+      return {};
+    }
+  }
+
+  async makeUserAlbumRequest(link = null) {
+    try {
+      const { data } = await this.axios.get(link || `/me/albums`);
+      return data;
+    } catch (e) {
+      console.error("SPOTIFY ERROR:: cannot get user album data:", e.message);
+      console.error(e.config);
+      return {};
+    }
+  }
+
+  async makeUserLikedSongsRequest(link = null) {
+    try {
+      const { data } = await this.axios.get(link || `/me/tracks`);
+      return data;
+    } catch (e) {
+      console.error(
+        "SPOTIFY ERROR:: cannot get user liked songs data:",
+        e.message
+      );
+      console.error(e.config);
+      return {};
+    }
+  }
+
   async makePlaylistRequest(playlistId, link = null) {
     try {
       const { data } = await this.axios.get(
@@ -176,6 +430,20 @@ class Spotify {
       return data;
     } catch (e) {
       console.error("SPOTIFY ERROR:: cannot get album data:", e.message);
+      console.error(e.config);
+      return {};
+    }
+  }
+
+  async makeAudiosFeaturesRequest(trackIds) {
+    try {
+      const { data } = await this.axios.get("audio-features", {
+        params: { ids: trackIds.join(",") }
+      });
+
+      return data;
+    } catch (e) {
+      console.error("SPOTIFY ERROR:: cannot get audio data:", e.message);
       console.error(e.config);
       return {};
     }
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
+const http = require("http");
+const qs = require("querystring");
+const url = require("url");
+const open = require("open");
+const fs = require("fs");
+
 function formatSongData(item) {
   if (item.id === null) {
     return { title: "", artists: [""] };
@@ -22,6 +28,79 @@ function formatSongData(item) {
   return data;
 }
 
+async function openBrowserForAuth(
+  client_id,
+  scope,
+  show_dialog = true,
+  port = 8008
+) {
+  const spotifyUrl =
+    "https://accounts.spotify.com/authorize?" +
+    qs.stringify({
+      client_id,
+      response_type: "code",
+      redirect_uri: "http://localhost:8008/callback",
+      scope,
+      show_dialog
+    });
+
+  return new Promise(resolve => {
+    // create a server to send and reciecve the auth code in a promise
+    const server = http.createServer((req, res) => {
+      // parse url to collect pathname and query
+      const { pathname, query } = url.parse(req.url);
+
+      // for initial route, redirect to spotify for auth
+      if (pathname === "/") {
+        res.writeHead(302, { location: spotifyUrl });
+        return res.end();
+      }
+
+      // for callback get the code and kill the server
+      if (pathname === "/callback") {
+        const { code } = qs.parse(query);
+        res.write("Authorization Successful");
+
+        // kill server
+        res.end();
+        server.close(() => {
+          return resolve({ code });
+        });
+      }
+
+      res.end();
+    });
+
+    server.listen(port, () => {
+      console.log("Server running on :8008");
+      open("http://localhost:8008");
+    });
+  });
+}
+
+function cacheCode(key, value) {
+  const cache = JSON.parse(fs.readFileSync("./cache.json"));
+
+  cache[key] = value;
+
+  fs.writeFileSync("./cache.json", JSON.stringify(cache));
+  return true;
+}
+
+function readCache(key) {
+  try {
+    const file = fs.readFileSync("./cache.json");
+    const data = JSON.parse(file)[key];
+
+    return data;
+  } catch (e) {
+    return false;
+  }
+}
+
 module.exports = {
-  formatSongData
+  formatSongData,
+  openBrowserForAuth,
+  cacheCode,
+  readCache
 };
