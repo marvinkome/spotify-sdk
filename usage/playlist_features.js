82c8fdc6d69dbc7826fdcbaebae604dd7b9fcdff
Marvin Kome <marvinkome@gmail.com>
2020-12-04 12:50:25 +0100

    update playlist features json

--- usage/playlist_features.js
+++ usage/playlist_features.js
@@ -1,5 +1,5 @@
-const Spotify = require("../src");
 const authData = require("../auth.json");
+const { Spotify } = require("../src");
 const { writeFileSync } = require("fs");
 const { merge } = require("lodash");
 
@@ -10,7 +10,10 @@ async function main(playlistId = "6f3lchHmBQed8GnWmayLn6", name = "unknown") {
   const playlistData = await spotify.getPlaylistTracks(playlistId, (item) => ({
     id: item.id,
     name: item.name,
+    album: item.album.name,
     artist: item.artists.map((a) => a.name).join(","),
+    explicit: item.explicit,
+    popularity: item.popularity,
   }));
 
   const audioData = await spotify.getAudiosFeatures(
